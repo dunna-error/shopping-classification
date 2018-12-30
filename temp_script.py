bc6e2f32ef298477bcc04434d15e5b8ffc4061c1
yoonkt200 <yoonkt200@gmail.com>
2018-12-31 00:23:53 +0900

    term_vector_df -> dict

---
+++ temp_script.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import pickle
+
+df_term_vector = pd.concat([
+    pd.read_pickle('./data/df_product_train_dataset.pkl'),
+    pd.read_pickle('./data/df_product_dev_dataset.pkl'),
+    pd.read_pickle('./data/df_product_test_dataset.pkl')],
+    axis=0)
+
+term_vector_dict = pd.Series(df_term_vector.pid.values, index=df_term_vector.term_vector).to_dict()
+
+with open('./data' + 'term_vector_dict.pickle', 'wb') as f:
+    pickle.dump(term_vector_dict, f, pickle.HIGHEST_PROTOCOL)
